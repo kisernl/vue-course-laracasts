e80bd31a2ecce9bd66516e9c6c9d13c93de5ed73
Noah Kiser <noah.kiser@gmail.com>
2024-07-23 22:19:44 -0500

    get tailwind css working on project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const assignments = ref([
+  { name: "Finish project", complete: false },
+  { name: "Read Chapter 4", complete: false },
+  { name: "Turn in homework", complete: false },
+]);
+</script>
 
 <template>
   <section>
-    <h2>Assignments</h2>
+    <h2 class="font-bold mb-2">Assignments</h2>
 
     <ul>
-      <li>Finish project</li>
-      <li>Read chapter 4</li>
-      <li>Turn in homework</li>
+      <li v-for="assignment in assignments">
+        <label>
+          {{ assignment.name }}
+          <input type="checkbox" v-model="assignment.complete" />
+        </label>
+      </li>
     </ul>
+    <pre>
+      {{ assignments }}
+    </pre>
   </section>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./assets/main.css";
 
 import { createApp } from "vue";
-import App from "./EpisodeTwo.vue";
+import App from "./App.vue";
 
 createApp(App).mount("#app");
